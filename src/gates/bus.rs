2ebba261b6d5d13efcfb69510120b94269de0df0
louis <louis.snoriguzzi@epitech.eu>
2026-06-10 09:59:14 +0200

    feat(p1): implement mux16 gate

--- src/gates/bus.rs
+++ src/gates/bus.rs
@@ -1,4 +1,5 @@
 use super::elementary::{and, not, or};
+use super::mux::mux;
 
 pub fn not16(input: [bool; 16]) -> [bool; 16] {
     input.map(not)
@@ -10,6 +11,10 @@ pub fn and16(a: [bool; 16], b: [bool; 16]) -> [bool; 16] {
 
 pub fn or16(a: [bool; 16], b: [bool; 16]) -> [bool; 16] {
     std::array::from_fn(|i| or(a[i], b[i]))
+}
+
+pub fn mux16(a: [bool; 16], b: [bool; 16], sel: bool) -> [bool; 16] {
+    std::array::from_fn(|i| mux(a[i], b[i], sel))
 }
 
 #[cfg(test)]
@@ -74,6 +79,50 @@ mod tests {
             [
                 true, true, false, true, true, true, false, true, true, true, false, true, true,
                 true, false, true
+            ],
+        );
+    }
+
+    #[test]
+    fn test_mux16() {
+        assert_eq!(mux16([false; 16], [false; 16], false), [false; 16]);
+        assert_eq!(mux16([false; 16], [false; 16], true), [false; 16]);
+        assert_eq!(mux16([true; 16], [false; 16], false), [true; 16]);
+        assert_eq!(mux16([true; 16], [false; 16], true), [false; 16]);
+        assert_eq!(mux16([false; 16], [true; 16], false), [false; 16]);
+        assert_eq!(mux16([false; 16], [true; 16], true), [true; 16]);
+        assert_eq!(
+            mux16(
+                [
+                    false, true, false, true, false, true, false, true, false, true, false, true,
+                    false, true, false, true
+                ],
+                [
+                    true, true, false, false, true, true, false, false, true, true, false, false,
+                    true, true, false, false
+                ],
+                false,
+            ),
+            [
+                false, true, false, true, false, true, false, true, false, true, false, true,
+                false, true, false, true
+            ],
+        );
+        assert_eq!(
+            mux16(
+                [
+                    false, true, false, true, false, true, false, true, false, true, false, true,
+                    false, true, false, true
+                ],
+                [
+                    true, true, false, false, true, true, false, false, true, true, false, false,
+                    true, true, false, false
+                ],
+                true,
+            ),
+            [
+                true, true, false, false, true, true, false, false, true, true, false, false, true,
+                true, false, false
             ],
         );
     }

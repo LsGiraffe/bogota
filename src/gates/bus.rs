e3783dae58918b943b1a863bfe2bae02c7b31fa6
louis <louis.snoriguzzi@epitech.eu>
2026-06-10 09:53:59 +0200

    feat(p1): implement and16 gate

--- src/gates/bus.rs
+++ src/gates/bus.rs
@@ -1,7 +1,11 @@
-use super::elementary::not;
+use super::elementary::{and, not};
 
 pub fn not16(input: [bool; 16]) -> [bool; 16] {
     input.map(not)
+}
+
+pub fn and16(a: [bool; 16], b: [bool; 16]) -> [bool; 16] {
+    std::array::from_fn(|i| and(a[i], b[i]))
 }
 
 #[cfg(test)]
@@ -21,6 +25,29 @@ mod tests {
                 true, false, true, false, true, false, true, false, true, false, true, false, true,
                 false, true, false
             ]
+        );
+    }
+
+    #[test]
+    fn test_and16() {
+        assert_eq!(and16([false; 16], [false; 16]), [false; 16]);
+        assert_eq!(and16([true; 16], [false; 16]), [false; 16]);
+        assert_eq!(and16([true; 16], [true; 16]), [true; 16]);
+        assert_eq!(
+            and16(
+                [
+                    false, true, false, true, false, true, false, true, false, true, false, true,
+                    false, true, false, true
+                ],
+                [
+                    true, true, false, false, true, true, false, false, true, true, false, false,
+                    true, true, false, false
+                ],
+            ),
+            [
+                false, true, false, false, false, true, false, false, false, true, false, false,
+                false, true, false, false
+            ],
         );
     }
 }

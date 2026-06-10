ff5c399f83a7082a72651a76b2b6da6243f9c775
louis <louis.snoriguzzi@epitech.eu>
2026-06-10 09:57:07 +0200

    feat(p1): implement or16 gate

--- src/gates/bus.rs
+++ src/gates/bus.rs
@@ -1,4 +1,4 @@
-use super::elementary::{and, not};
+use super::elementary::{and, not, or};
 
 pub fn not16(input: [bool; 16]) -> [bool; 16] {
     input.map(not)
@@ -6,6 +6,10 @@ pub fn not16(input: [bool; 16]) -> [bool; 16] {
 
 pub fn and16(a: [bool; 16], b: [bool; 16]) -> [bool; 16] {
     std::array::from_fn(|i| and(a[i], b[i]))
+}
+
+pub fn or16(a: [bool; 16], b: [bool; 16]) -> [bool; 16] {
+    std::array::from_fn(|i| or(a[i], b[i]))
 }
 
 #[cfg(test)]
@@ -47,6 +51,29 @@ mod tests {
             [
                 false, true, false, false, false, true, false, false, false, true, false, false,
                 false, true, false, false
+            ],
+        );
+    }
+
+    #[test]
+    fn test_or16() {
+        assert_eq!(or16([false; 16], [false; 16]), [false; 16]);
+        assert_eq!(or16([false; 16], [true; 16]), [true; 16]);
+        assert_eq!(or16([true; 16], [true; 16]), [true; 16]);
+        assert_eq!(
+            or16(
+                [
+                    false, true, false, true, false, true, false, true, false, true, false, true,
+                    false, true, false, true
+                ],
+                [
+                    true, true, false, false, true, true, false, false, true, true, false, false,
+                    true, true, false, false
+                ],
+            ),
+            [
+                true, true, false, true, true, true, false, true, true, true, false, true, true,
+                true, false, true
             ],
         );
     }
